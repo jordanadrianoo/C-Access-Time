ab419ecd612864fd69f023d8e2d75b770b452c82
Jordan Adriano <56409204+jordanadrianoo@users.noreply.github.com>
2020-03-02 08:44:35 -0600

    Update myTime.cpp

--- myTime.cpp
+++ myTime.cpp
@@ -11,19 +11,34 @@ int main()
 	clock_t start;
 	clock_t stop;
 	double elapsed_time;
+	int delay = 10000000;
 
 	int nums[] = { 1,2,3,4,5 };
 	vector<int> num = { 1,2,3,4,5 };
 
-
+	cout << "Array Access Time ";;
 	start = clock();
 
-	for (int i = 0; i < 100000000; i++)
+	for (int i = 0; i < delay; i++)
 	{
-
+		nums[0];
 	}
 	stop = clock();
 
 	elapsed_time = double(stop - start)/CLOCKS_PER_SEC;
 	cout << elapsed_time << endl;
+
+
+	cout << "Vector Access Time ";;
+	start = clock();
+
+	for (int i = 0; i < delay; i++)
+	{
+		num[0];
+	}
+	stop = clock();
+
+	elapsed_time = double(stop - start) / CLOCKS_PER_SEC;
+	cout << elapsed_time << endl;
+	system("pause");
 }
